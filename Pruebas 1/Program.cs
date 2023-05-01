ba4d4724f22f281f0faff9bdffc007f3b2fc4249
jeico <jeico@DESKTOP-JQT01V0>
2023-05-01 17:10:53 -0500

    Agregar archivos de proyecto.

---
+++ Pruebas 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite un numero porfavor: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            // Si el numero es un numero par o impar se divide y el residual si da 0 entonces es un numero par si no es un numero impar
+
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("El numero " + num + " Es par.");
+            }
+
+            else
+            {
+                Console.WriteLine("El numero " + num + "Es impar.");
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
