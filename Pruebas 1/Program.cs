3b07ca9b337687f907360830c25497d1efcb7f75
johan.dominguez <johan.dominguez@C20-INN-005>
2023-05-03 11:16:58 -0500

    Nombres y edades

--- Pruebas 1/Program.cs
+++ Pruebas 1/Program.cs
@@ -11,73 +11,87 @@ namespace Pruebas_1
         static void Main(string[] args)
         {
             #region numeros primos
-            Console.WriteLine("Digite un numero porfavor: ");
+            //Console.WriteLine("Digite un numero porfavor: ");
 
-            int num = int.Parse(Console.ReadLine());
+            //int num = int.Parse(Console.ReadLine());
 
-            // Si el numero es un numero par o impar se divide y el residual si da 0 entonces es un numero par si no es un numero impar
+            //// Si el numero es un numero par o impar se divide y el residual si da 0 entonces es un numero par si no es un numero impar
 
-            if ((num % 2) == 0)
-            {
-                Console.WriteLine("El numero " + num + " Es par.");
-            }
+            //if ((num % 2) == 0)
+            //{
+            //    Console.WriteLine("El numero " + num + " Es par.");
+            //}
 
-            else
-            {
-                Console.WriteLine("El numero " + num + " Es impar.");
-            }
+            //else
+            //{
+            //    Console.WriteLine("El numero " + num + " Es impar.");
+            //}
 
-            Console.ReadKey();
+            //Console.ReadKey();
 
             #endregion
 
             #region Dias de la semana
 
-            Console.WriteLine("Por favor digite un numero de la semana: ");
+            //Console.WriteLine("Por favor digite un numero de la semana: ");
 
-            int numMes = int.Parse(Console.ReadLine());
+            //int numMes = int.Parse(Console.ReadLine());
 
-            switch (numMes)
-            {
-                case 1:
-                    Console.WriteLine("Dia de la semnana escogido: Lunes");
-                    break;
+            //switch (numMes)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Dia de la semnana escogido: Lunes");
+            //        break;
 
-                case 2:
-                    Console.WriteLine("Dia de la semnana escogido: Martes");
-                    break;
+            //    case 2:
+            //        Console.WriteLine("Dia de la semnana escogido: Martes");
+            //        break;
 
-                case 3:
-                    Console.WriteLine("Dia de la semnana escogido: Miercoles");
-                    break;
+            //    case 3:
+            //        Console.WriteLine("Dia de la semnana escogido: Miercoles");
+            //        break;
 
-                case 4:
-                    Console.WriteLine("Dia de la semnana escogido: Jueves");
-                    break;
+            //    case 4:
+            //        Console.WriteLine("Dia de la semnana escogido: Jueves");
+            //        break;
 
-                case 5:
-                    Console.WriteLine("Dia de la semnana escogido: Viernes");
-                    break;
+            //    case 5:
+            //        Console.WriteLine("Dia de la semnana escogido: Viernes");
+            //        break;
 
-                case 6:
-                    Console.WriteLine("Dia de la semnana escogido: Sabado");
-                    break;
+            //    case 6:
+            //        Console.WriteLine("Dia de la semnana escogido: Sabado");
+            //        break;
 
-                case 7:
-                    Console.WriteLine("Dia de la semnana escogido: Domingo");
-                    break;
-            }
-            Console.ReadKey();
+            //    case 7:
+            //        Console.WriteLine("Dia de la semnana escogido: Domingo");
+            //        break;
+            //}
+            //Console.ReadKey();
             #endregion
 
             #region Tablas de multiplicar
+
+            //Console.WriteLine("Por favor indique que tabla de multiplicar desea obtener informacion.");
             
-            
-            
+            //int NumTablaMulti = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Tabla de Multiplicar del numero " + NumTablaMulti + ":");
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int resultado = NumTablaMulti * i;
+            //    Console.WriteLine(NumTablaMulti + " x " + i + " = " + resultado);
+            //}
+
+            //Console.ReadKey();
+
             #endregion
+
+            #region Nombres y edades
+
+            #endregion
+
         }
-
-
     }
-
 }
